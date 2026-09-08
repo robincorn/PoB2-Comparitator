a112b05b30ceda70a6c168e0a68be65b4866fb03
robincorn <dthp22mn57@privaterelay.appleid.com>
2026-09-08 17:49:04 +0200

    Expose clipboard build loading to renderer

--- src/preload.js
+++ src/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('pob', {
   status: () => ipcRenderer.invoke('bridge-status'),
   selectBuild: () => ipcRenderer.invoke('select-build'),
+  loadClipboardBuild: () => ipcRenderer.invoke('load-clipboard-build'),
   calculate: () => ipcRenderer.invoke('calculate'),
   onBridgeStatus: (callback) => ipcRenderer.on('bridge-status', (_event, value) => callback(value)),
 });
