12030f78c0eb69625b7a493feb01606e89f138d3
robincorn <dthp22mn57@privaterelay.appleid.com>
2026-09-08 17:56:56 +0200

    feat: keep overlay alive when hidden

--- src/preload.js
+++ src/preload.js
@@ -5,5 +5,6 @@ contextBridge.exposeInMainWorld('pob', {
   selectBuild: () => ipcRenderer.invoke('select-build'),
   loadClipboardBuild: () => ipcRenderer.invoke('load-clipboard-build'),
   calculate: () => ipcRenderer.invoke('calculate'),
+  hideOverlay: () => ipcRenderer.invoke('hide-overlay'),
   onBridgeStatus: (callback) => ipcRenderer.on('bridge-status', (_event, value) => callback(value)),
 });
