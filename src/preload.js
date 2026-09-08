04483c7d6d9e14fd567c3d66ef48560997d70d0f
robincorn <dthp22mn57@privaterelay.appleid.com>
2026-09-08 18:07:26 +0200

    Expose item comparison IPC

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('pob', {
   status: () => ipcRenderer.invoke('bridge-status'),
   selectBuild: () => ipcRenderer.invoke('select-build'),
   loadClipboardBuild: () => ipcRenderer.invoke('load-clipboard-build'),
+  compareClipboardItem: () => ipcRenderer.invoke('compare-clipboard-item'),
   calculate: () => ipcRenderer.invoke('calculate'),
   hideOverlay: () => ipcRenderer.invoke('hide-overlay'),
   onBridgeStatus: (callback) => ipcRenderer.on('bridge-status', (_event, value) => callback(value)),
